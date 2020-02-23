b0401ab75aad8843fb158ed9c549ad0e9ec747ee
Sergienko Anton <sergienkoanton@mail.ru>
2020-02-23 14:05:22 +0300

    Add first version

---
+++ optimize.js
@@ -0,0 +1,26 @@
+const imagemin = require("imagemin");
+const mozjpeg = require("imagemin-mozjpeg");
+const pngquant = require("imagemin-pngquant");
+const giflossy = require("imagemin-giflossy");
+const svgo = require("imagemin-svgo");
+const webp = require("imagemin-webp");
+
+(async () => {
+  let path = process.argv.slice(2)[0];
+  path = !!path ? path : "images/";
+  if (path.substr(-1) != "/") path += "/";
+
+  const files = await imagemin([path + "*.{jpg,jpeg,png,gif,svg,webp}"], path + "/optimized/", {
+    plugins: [
+      mozjpeg({ quality: 90, progressive: true }),
+      pngquant(),
+      giflossy(),
+      svgo({
+        plugins: [{ removeUnknownsAndDefaults: false }, { removeRasterImages: true }, {cleanupIDs: { force: true } }]
+      })
+      //webp({quality: 95})
+    ]
+  });
+
+  console.log(files);
+})();
