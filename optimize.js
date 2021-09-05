784852886a4c06d032141bc602e2eb742127af6b
Sergienko Anton <sergienkoanton@mail.ru>
2021-09-05 20:24:12 +0300

    Update imagemin from 7.0.1 to 8.0.1

--- optimize.js
+++ optimize.js
@@ -1,34 +1,37 @@
-const imagemin = require("imagemin");
-const mozjpeg = require("imagemin-mozjpeg");
-const pngquant = require("imagemin-pngquant");
-const giflossy = require("imagemin-giflossy");
-const svgo = require("imagemin-svgo");
-const { extendDefaultPlugins } = require("svgo");
-const webp = require("imagemin-webp");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminGifsicle = require("imagemin-gifsicle");
+const imageminWebp = require("imagemin-webp");
+const imageminSvgo = require('imagemin-svgo');
+const {extendDefaultPlugins} = require('svgo');
 
-(async () => {
-  let path = process.argv.slice(2)[0];
-  path = !!path ? path : "images/";
-  if (path.substr(-1) != "/") path += "/";
+let path = process.argv.slice(2)[0];
+path = !!path ? path : "images/";
+if (path.substr(-1) != "/") path += "/";
 
-  destinationPath = path + "/optimized/";
+destinationPath = path + "/optimized/";
 
-  const files = await imagemin([path + "*.{jpg,jpeg,png,gif,svg,webp}"], {
-    destination: destinationPath,
-    plugins: [
-      mozjpeg({ quality: 90, progressive: true }),
-      pngquant(),
-      giflossy(),
-      svgo({
-        plugins: extendDefaultPlugins([
-          { name: "removeUnknownsAndDefaults", active: false },
-          { name: "removeRasterImages", active: true },
-          { name: "cleanupIDs", force: true },
-        ]),
-      }),
-      //webp({ quality: 95 }),
-    ],
-  });
+function optimizeFiles(extension, imageminPlugin) {
+  (async () => {
+    const imagemin = (await import("imagemin")).default;
+    const files = await imagemin([path + extension], {
+      destination: destinationPath,
+      plugins: [imageminPlugin],
+    });
+    console.log(extension + ":");
+    console.log(files);
+  })();
+}
 
-  console.log(files);
-})();
+optimizeFiles("*.{jpg,jpeg}", imageminMozjpeg({ quality: 90, progressive: true }));
+// optimizeFiles("*.{jpg,jpeg}", imageminWebp({ quality: 80, method: 6 }));
+optimizeFiles("*.png", imageminPngquant());
+optimizeFiles("*.gif", imageminGifsicle());
+optimizeFiles("*.webp", imageminWebp({ quality: 80, method: 6 }));
+optimizeFiles("*.svg", imageminSvgo({
+  plugins: extendDefaultPlugins([
+    { name: "removeUnknownsAndDefaults", active: false },
+    { name: "removeRasterImages", active: true },
+    { name: "cleanupIDs", force: true },
+  ]),
+}));
